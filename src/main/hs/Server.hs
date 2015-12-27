ecdd253d9bc62c9e380148ef27af9aa06a1a8018
niklasf <niklasf@cs.umu.se>
2015-12-27 19:13:14 +0100

    added default response

--- src/main/hs/Server.hs
+++ src/main/hs/Server.hs
@@ -1,4 +1,26 @@
 module Main where 
 
+import Network.Socket
+
+defaultResponse :: String
+defaultResponse = "HTTP/1.0 200 OK\r\nContent-Length: 3\r\n\r\nhej"
+
 main :: IO ()
-main = putStrLn "hej"
+main = do
+    sock <- serverSocket 8080
+    acceptLoop sock
+
+serverSocket :: PortNumber -> IO Socket
+serverSocket port = do
+    sock <- socket AF_INET Stream defaultProtocol
+    setSocketOption sock ReuseAddr 1
+    bindSocket sock (SockAddrInet port iNADDR_ANY)
+    listen sock 2
+    return sock
+
+acceptLoop :: Socket -> IO ()
+acceptLoop sock = do
+    (cliSock, _) <- accept sock
+    _ <- send cliSock defaultResponse
+    acceptLoop sock
+
