6d1e01ea602552e6964e961bd00ab089718c7ba2
Magnus Fries <magnus.fries@gmail.com>
2016-01-03 21:52:18 +0100

    Updated testsite

--- resources/testsite/script.js
+++ resources/testsite/script.js
@@ -1,5 +1,28 @@
 
+function readFile(path) {
+    
+    var text = "", file = new XMLHttpRequest();
+    
+    file.open("GET", path, false);
+    
+    file.onreadystatechange = function () {
+        if (file.readyState == 4) {
+            if (file.status == 200) {
+                text = file.responseText;
+            }
+        }
+    };
+    
+    file.send(null);
+    
+    return text;
+}
+
 // All scriptning som ska gras i brjan placeras hr
 function init() {
-    document.getElementById("content").innerHTML = "Javascript fungerar";
+
+	var content = document.getElementById("content");
+
+    content.innerHTML = "Javascript: OK";
+    content.innerHTML += readFile("readfile.txt");
 }
